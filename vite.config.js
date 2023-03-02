f8689ad51cbdacbe4fe2cb1b6c4307c03f24b332
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-03-02 11:07:26 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-vite/",
+  base: "https://github.com/IvanHenriquez96/Portfolio-vite/",
+  // base: "/Portfolio-vite/",
   // base: "https://IvanHenriquez96.github.io/Portfolio-vite/",
 });
