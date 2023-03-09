039ed5ea93cfafa78db22032acace1e4cb55bf2f
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-03-09 17:54:06 -0300

    config netlify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-vite/",
-  // base: "https://IvanHenriquez96.github.io/Portfolio-vite/",
+  // base: "/Portfolio-vite/", //para subirlo a Netlify hay que quitar este base
 });
